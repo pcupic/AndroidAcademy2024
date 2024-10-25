eb51e324d752d60d94b3506c16fc8114ba8af53d
pcupic <cupicpatrik@gmail.com>
2024-10-25 18:15:05 +0200

    Values and variables

---
+++ src/main/kotlin/ValuesAndVariables.kt
@@ -0,0 +1,26 @@
+fun task1() {
+    /* Napravite varijable za godinu vašeg rođenja i trenutnu godinu.
+    Izračunajte koliko godina imate. Izračunajte koliko ćete godina imati 2048. */
+    var yearOfBirth = 2002
+    var currentYear = 2024
+    var currentAge = currentYear - yearOfBirth
+    println("You are $currentAge old")
+    var ageIn2048 = 2048 - yearOfBirth
+    println("You will be $ageIn2048 old in 2048")
+}
+
+fun task2() {
+    /* Napravite tri vrijednosti, za Vaše i imena dvaju osoba pokraj Vas na akademiji. Ispišite
+    poruke u kojoj napominjete tko sjedi lijevo, a tko desno od koga. */
+    var myName = "Patrik"
+    var leftNeighbour = "Matko"
+    var rightNeighbour = "Ante"
+    println("Patrik's left neighbour is $leftNeighbour and right neighbour is $rightNeighbour. \n" +
+            "Matko's left neighbour is unknown, but his right neighbour is $myName. \n" +
+            "Ante's right neighbour is unknown, but his left neighbour is $myName")
+}
+
+fun main(args: Array<String>) {
+    task1()
+    task2()
+}
